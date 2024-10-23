55faf2cdd9bf38a0d67f7814d4f5a019046e5290
Jackson Delahunt <jackson@stemn.com>
2024-10-24 05:16:40 +1100

    no-alias-keyword

--- .config/zsh/dependencies.zsh
+++ .config/zsh/dependencies.zsh
@@ -43,7 +43,7 @@ install_github_releases() {
     dpkg-deb -x $HOME/dra.deb $HOME/.apt && \
     rm $HOME/dra.deb
 
-  alias dra="dra download --automatic --install --output $HOME/.local/bin"
+  dra="dra download --automatic --install --output $HOME/.local/bin"
 
   dra eza-community/eza &
   dra fastfetch-cli/fastfetch &
