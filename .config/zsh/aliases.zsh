b7b17b6959bf1a652d106815d3f58537adb5a634
Jackson Delahunt <jackson@stemn.com>
2026-01-31 18:42:56 +1100

    alternative-binary-alias

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -97,7 +97,6 @@ alias asciic-blocks="command asciic --colorize --use-system-binaries"
 alias au="sudo apt-get update"
 alias ax="arc unarchive"
 alias bat="batcat --style plain --theme ansi"
-alias cat=bat
 alias cbonsai="cbonsai --seed 119 --live --infinite"
 alias clip=clipboard
 alias code="cursor $@"
@@ -164,3 +163,7 @@ alias zl="clear; zsh --login"
 
 # Run extension with program
 alias -s pdf='zathura'
+
+# Alias alternative binaries if available
+command -v bat >/dev/null && alias cat=bat || true
+command -v nala >/dev/null && alias apt-get=nala || true
