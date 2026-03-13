6f5ebad91620c383792aca9d5e26b867b31a5511
Jackson Delahunt <jackson@stemn.com>
2026-03-13 17:00:42 +1100

    claude-unset-anthropic

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -99,7 +99,7 @@ alias ax="arc unarchive"
 alias bat="batcat --style plain --theme ansi"
 alias cat=bat
 alias cbonsai="cbonsai --seed 119 --live --infinite"
-alias claude="claude --dangerously-skip-permissions"
+alias claude="ANTHROPIC_API_KEY= claude --dangerously-skip-permissions"
 alias clip=clipboard
 alias code="cursor $@"
 alias de="docker exec --interactive --tty"
