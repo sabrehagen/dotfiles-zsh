1698bf1e94160eac57da1a7a05d09223aba672e8
Jackson Delahunt <jackson@stemn.com>
2026-04-16 00:00:32 +1000

    silence-on-not-found

--- .config/zsh/exports.zsh
+++ .config/zsh/exports.zsh
@@ -1,8 +1,8 @@
 # Load jump functions
-eval "$(jump shell zsh)"
+eval "$(jump shell zsh 2>/dev/null)"
 
 # Load mise
-eval "$(mise activate zsh)"
+eval "$(mise activate zsh 2>/dev/null)"
 
 # zsh-history-substring-search plugin configuration
 HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_TIMEOUT=0.3
